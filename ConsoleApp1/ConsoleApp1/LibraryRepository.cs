e04758e125e90988d597636c31b7e2b1cd7489a4
mahammadalirk <mahammadalirk@code.edu.az>
2022-07-20 07:52:59 +0400

    ConsoleApp1 file

---
+++ ConsoleApp1/ConsoleApp1/LibraryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class LibraryRepository
+    {
+        internal void Create(Library library)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update(Library library)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Library> GetAll(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Delete(Library library)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Library> GetAll(object p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal List<Library> GetAll(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
